23dd16bbb705b0f8e6cde0a45d6025cda5b5b11e
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-24 11:35:08 +0530

    Completed challenge 10_leapYears of the foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear = (year % 4 == 0) ? 
+    (year % 400 == 0) ? true : 
+    (year % 100 == 0) ? false : true 
+    : false;
 
+    return leapYear;
 };
 
 // Do not edit below this line
