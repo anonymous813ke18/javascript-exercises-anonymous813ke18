13cda9f7dc330d4340873746fb5db53d9a4a2bb5
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-29 14:33:55 +0530

    Passed all three addition test

--- foundations/12_calculator/calculator.spec.js
+++ foundations/12_calculator/calculator.spec.js
@@ -5,11 +5,11 @@ describe('add', () => {
     expect(calculator.add(0, 0)).toBe(0);
   });
 
-  test.skip('adds 2 and 2', () => {
+  test('adds 2 and 2', () => {
     expect(calculator.add(2, 2)).toBe(4);
   });
 
-  test.skip('adds positive numbers', () => {
+  test('adds positive numbers', () => {
     expect(calculator.add(2, 6)).toBe(8);
   });
 });
@@ -20,11 +20,11 @@ describe('subtract', () => {
   });
 
   test.skip('subtracts negative numbers', () => {
-    expect(calculator.subtract(-10, -4)).toBe(-6);
+    expect(calculator.subtract(-10, -4)).toBe(-14);
   });
 
   test.skip('subtracts numbers of mixed parity', () => {
-    expect(calculator.subtract(-8, 7)).toBe(-15);
+    expect(calculator.subtract(-8, 7)).toBe(-1);
   });
 });
 
