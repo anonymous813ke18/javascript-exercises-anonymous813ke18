dfd724af36b2fd93aaf7af54f0fe9dab93335229
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-29 15:57:17 +0530

    Used for loop to calculate fibonacci series

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (!Number.isInteger(num)) num = Number.parseInt(num);
+    if (num === 0) return 0;
+    if (num < 0) return 'OOPS';
 
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= num; i++) {
+        let total = a + b;
+        a = b;
+        b = total;
+    }
+    return (b);
 };
 
 // Do not edit below this line
