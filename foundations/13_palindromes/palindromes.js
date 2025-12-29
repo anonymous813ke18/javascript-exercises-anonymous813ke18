4236d9071968683e319a74d3b5a6c22c2eaaea1a
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-29 15:11:32 +0530

    Used string functions like toLowerCase and split, along with array functions like reverse, filter and join to complete the 13_Palindrome exercise in the foundations. Also, used regex for alpha-numeric values.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reversedStr = str
+                .toLowerCase()
+                .split("")
+                .reverse()
+                .filter((word) => word.match(/[a-z0-9]/i))
+                .join('');
+    let cleanedStr = str
+                .toLowerCase()
+                .split("")
+                .filter((word) => word.match(/[a-z0-9]/i))
+                .join('');
+    
+    return reversedStr === cleanedStr;
 };
 
 // Do not edit below this line
