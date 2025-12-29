632936ba0053b2c4446560507348b992268982fc
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-29 16:06:20 +0530

    Used the map function to get the book titles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookNames = books.map(book => book.title);
+    return bookNames;
 };
 
 // Do not edit below this line
